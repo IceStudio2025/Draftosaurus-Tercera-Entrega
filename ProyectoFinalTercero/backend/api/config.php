b8e6c15a2ff9fa1957b6ddbeab9d394270382129
IceStudio2025 <icestudio.oficial@gmail.com>
2025-11-17 20:21:33 -0300

    1
    1

--- ProyectoFinalTercero/backend/api/config.php
+++ ProyectoFinalTercero/backend/api/config.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 $OPENAI_KEY = '';
-=======
-$OPENAI_KEY = '';
->>>>>>> f415a43d2dc8d8cfc28a4ce98580146a2da4ba7b
 
 $OPENAI_MODEL = 'meta-llama/llama-4-scout-17b-16e-instruct';
 
